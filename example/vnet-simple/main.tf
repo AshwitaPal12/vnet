4efa70e682f66d94798f14fe32e322032960ca18
Ashwita Pal <ashwita.pal@tothenew.com>
2023-08-18 10:39:19 +0530

    change source

--- example/vnet-simple/main.tf
+++ example/vnet-simple/main.tf
@@ -4,7 +4,8 @@ provider "azurerm" {
 
 module "vnet_main" {
   #source = "git::https://github.com/tothenew/terraform-azure-vnet.git?ref=vnet-v1"
-  source = "../.."
+  #source = "../.."
+  source = "git::https://github.com/AshwitaPal12/vnet.git"
   address_space  = "10.0.0.0/16"
   subnet_type = "subnet_simple" 
   subnet_bits = 3 
